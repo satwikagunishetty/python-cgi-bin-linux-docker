ae07e3eb5c2af8a8726453b78ed870059e72927b
satwika gunishetty <60072014+satwikagunishetty@users.noreply.github.com>
2020-09-28 14:33:21 +0530

    Create dockerstop.py

---
+++ dockerstop.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+print("content-type: text/html")
+print()
+
+import subprocess as sp
+import cgi
+
+form=cgi.FieldStorage()
+
+osname=form.getvalue("do")
+
+
+cmd="sudo docker stop {0}".format(osname)
+
+output=sp.getstatusoutput(cmd)
+print(output)
+status=output[0]
+out=output[1]
+
+if status==0:
+  print("os stopped {} ..".format(osname))
+else:
+  print("some error : {}".format(out))
