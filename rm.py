ff15434976ccb879790d9d47598ed077b94d3121
satwika gunishetty <60072014+satwikagunishetty@users.noreply.github.com>
2020-09-28 14:37:21 +0530

    Create rm.py

---
+++ rm.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+print("content-type: text/html")
+print()
+
+import os
+import subprocess as sp
+import cgi
+
+form=cgi.FieldStorage()
+x=form.getvalue("o")
+
+a="sudo rm "+x
+cmd=sp.getoutput(a)
+
+output=sp.getstatusoutput(cmd)
+
+status=output[0]
+out=output[1]
+
+
+if status==0:
+  print("{} is deleted successfully ..".format(x))
+else:
+  print("{}".format(out))
